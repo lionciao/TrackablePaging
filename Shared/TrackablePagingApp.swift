6fe85ea84f6cca1320fba8c6a8134f13d5af9d2a
lionciao <lionciao16@gmail.com>
2023-03-10 16:40:59 +0800

    Initial Commit

---
+++ Shared/TrackablePagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackablePagingApp.swift
+//  Shared
+//
+//  Created by Ciao on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct TrackablePagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
